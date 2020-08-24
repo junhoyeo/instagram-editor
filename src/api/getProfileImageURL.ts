3bb7a7c1c598bd000c8e8905556e130faf9437bf
Junho Yeo <hanaro0704@gmail.com>
2020-08-25 08:44:29 +0900

    fix(api): bypass instagram auth with google image proxy

--- src/api/getProfileImageURL.ts
+++ src/api/getProfileImageURL.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 export default async function getProfileImageURL(username: string) {
-  const {
-    data: {
+  const { data: html } = await axios.get(`https://images${~~(Math.random()*3333)}-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https://www.instagram.com/${username}/`);
+  const regex = /_sharedData = ({.*);<\/script>/m;
+  const regexResults = regex.exec(html);
+  if (regexResults) {
+    const json = JSON.parse(regexResults[1]);
+    const {
       graphql: {
         user: {
           profile_pic_url: profileImageURL,
         },
       },
-    },
-  } = await axios.get(`https://www.instagram.com/${username}/?__a=1`);
-  return profileImageURL;
+    } = json.entry_data.ProfilePage[0];
+    return profileImageURL;
+  }
 }
